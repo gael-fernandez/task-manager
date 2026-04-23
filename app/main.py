0d23050eba58b76a7917fafc491fc2eb765a1e09
Gael Osbaldo Fernandez Garcia <gael.fernandez.dev@gmail.com>
2026-04-23 10:26:11 -0600

    Add CORS configuration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from app.routers import usuarios,tareas
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import usuarios, tareas
 import app.models
-from app.database import engine,Base
-from app.models import Usuario,Tarea
+from app.database import engine, Base
+from app.models import Usuario, Tarea
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -10,5 +12,18 @@ app = FastAPI(
     description="API de gestión de tareas con autenticación JWT",
     version="1.0.0"
 )
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(usuarios.router)
 app.include_router(tareas.router)
