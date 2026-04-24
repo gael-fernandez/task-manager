a71fd6156b550cbb9215f7c30ef01d40927c5833
Gael Osbaldo Fernandez Garcia <gael.fernandez.dev@gmail.com>
2026-04-23 21:37:00 -0600

    update cors

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 origins = [
     "http://localhost:5173",
-    "https://frontend-task-manager-proyecto-de-p.vercel.app/"
+    "https://frontend-task-manager-proyecto-de-port-gael-fernandezs-projects.vercel.app/"
 ]
 
 app.add_middleware(
